d799c12153db28b28531d6ff1ac232c139e0132d
TamirisCarv <tamiris.carvalho04@gmail.com>
2023-06-04 09:59:55 -0300

    Inserindo o campo código de validade

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("numero").addEventListener("keyup", function(event) {
+document.getElementById("numero").addEventListener("keyup", function (event) {
     let numero = document.getElementById("numero").value;
     let exibirnumero = document.getElementById("exibirnumero");
 
@@ -8,13 +8,22 @@ document.getElementById("numero").addEventListener("keyup", function(event) {
     }
 });
 
-document.getElementById("nome").addEventListener("keyup", function(event) {
-  document.getElementById("exibirnome").innerHTML = document.querySelector("#nome")
-    .value.toUpperCase();
+document.getElementById("nome").addEventListener("keyup", function (event) {
+    document.getElementById("exibirnome").innerHTML = document.querySelector("#nome")
+        .value.toUpperCase();
 });
 
-document.getElementById("validade").addEventListener("keyup", function(event) {
-   
+document.getElementById("validade").addEventListener("keyup", function (event) {
     document.getElementById("exibirvalidade").innerHTML = document.querySelector("#validade")
-    .value.toUpperCase();
+        .value.toUpperCase();
+});
+
+document.getElementById("codigo").addEventListener("keyup", function (event) {
+    let codigo = document.getElementById("codigo").value;
+    let exibircodigo = document.getElementById("exibircodigo");
+
+    let asteriscoIndex = exibircodigo.innerHTML.indexOf("*");
+    if (asteriscoIndex !== -1) {
+        exibircodigo.innerHTML = exibircodigo.innerHTML.replace("*", codigo.charAt(0));
+    }
 });
