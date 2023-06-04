b9fd02ccb36481d9864b137ee0ad4ef4a0a0e666
TamirisCarv <tamiris.carvalho04@gmail.com>
2023-06-04 10:36:11 -0300

    Adicionando máscaras

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-document.getElementById("numero").addEventListener("keyup", function (event) {
+IMask(document.querySelector("#numero"), {
+    mask: "0000 0000 0000 0000",
+  });
+document.getElementById("numero").addEventListener("input", function (event) {
     let numero = document.getElementById("numero").value;
     let exibirnumero = document.getElementById("exibirnumero");
-
+  
     let asteriscoIndex = exibirnumero.innerHTML.indexOf("*");
     if (asteriscoIndex !== -1) {
-        exibirnumero.innerHTML = exibirnumero.innerHTML.replace("*", numero.charAt(0));
+        exibirnumero.innerHTML = exibirnumero.innerHTML.replace("*", numero.charAt(numero.length - 1));
+     
     }
-});
+  });
+  
 
 document.getElementById("nome").addEventListener("keyup", function (event) {
     document.getElementById("exibirnome").innerHTML = document.querySelector("#nome")
@@ -29,6 +34,20 @@ document.getElementById("codigo").addEventListener("keyup", function (event) {
     let asteriscoIndex = exibircodigo.innerHTML.indexOf("*");
     if (asteriscoIndex !== -1) {
         exibircodigo.innerHTML = exibircodigo.innerHTML.replace("*", codigo.charAt(0));
+    }
+});
+
+IMask(document.querySelector("#cpf"), {
+    mask: "000.000.000-00",
+  });
+
+document.getElementById("cpf").addEventListener("keyup", function (event) {
+    let cpf = document.getElementById("cpf").value;
+    let exibircpf = document.getElementById("exibircpf");
+
+    let asteriscoIndex = exibircpf.innerHTML.indexOf("*");
+    if (asteriscoIndex !== -1) {
+        exibircpf.innerHTML = exibircpf.innerHTML.replace("*", cpf.charAt(cpf.length - 1));
     }
 });
 
