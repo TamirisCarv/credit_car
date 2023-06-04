bca84634fae3aafc285ee640d86410a4c15e3fb4
TamirisCarv <tamiris.carvalho04@gmail.com>
2023-06-04 13:41:17 -0300

    Adicionando CSS

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 IMask(document.querySelector("#numero"), {
     mask: "0000 0000 0000 0000",
-  });
+});
 document.getElementById("numero").addEventListener("input", function (event) {
     let numero = document.getElementById("numero").value;
     let exibirnumero = document.getElementById("exibirnumero");
-  
+
     let asteriscoIndex = exibirnumero.innerHTML.indexOf("*");
     if (asteriscoIndex !== -1) {
         let primeiroDigito = numero.charAt(0);
         exibirnumero.innerHTML = exibirnumero.innerHTML.replace("*", numero.charAt(numero.length - 1));
-   
-       let imagem;
-       switch(primeiroDigito){
-        case '4':  imagem = document.querySelector("#exibiroperadora img");
-        if (!imagem) {
-          imagem = document.createElement("img");
-          imagem.src = "./imagens/visa.png"; 
-          document.querySelector("#exibiroperadora").appendChild(imagem); 
-        }
-        break;
 
-        case '5':  imagem = document.querySelector("#exibiroperadora img");
-        if (!imagem) {
-          imagem = document.createElement("img");
-          imagem.src = "./imagens/mastercard.png"; 
-          document.querySelector("#exibiroperadora").appendChild(imagem); 
-        }
-        break;
+        let imagem;
+        switch (primeiroDigito) {
+            case '4': imagem = document.querySelector("#exibiroperadora img");
+                if (!imagem) {
+                    imagem = document.createElement("img");
+                    imagem.src = "./imagens/visa.png";
+                    document.querySelector("#exibiroperadora").appendChild(imagem);
+                }
+                break;
 
-        case '6':  imagem = document.querySelector("#exibiroperadora img");
-        if (!imagem) {
-          imagem = document.createElement("img");
-          imagem.src = "./imagens/hipercard.png"; 
-          document.querySelector("#exibiroperadora").appendChild(imagem); 
-        }
-        break;
+            case '5': imagem = document.querySelector("#exibiroperadora img");
+                if (!imagem) {
+                    imagem = document.createElement("img");
+                    imagem.src = "./imagens/mastercard.png";
+                    document.querySelector("#exibiroperadora").appendChild(imagem);
+                }
+                break;
 
-       
+            case '6': imagem = document.querySelector("#exibiroperadora img");
+                if (!imagem) {
+                    imagem = document.createElement("img");
+                    imagem.src = "./imagens/hipercard.png";
+                    document.querySelector("#exibiroperadora").appendChild(imagem);
+                }
+                break;
+
+
         }
     }
-  });
-  
+});
 
 document.getElementById("nome").addEventListener("keyup", function (event) {
     document.getElementById("exibirnome").innerHTML = document.querySelector("#nome")
@@ -49,7 +48,7 @@ document.getElementById("nome").addEventListener("keyup", function (event) {
 
 IMask(document.querySelector("#validade"), {
     mask: "00/00",
-  });
+});
 
 document.getElementById("validade").addEventListener("keyup", function (event) {
     document.getElementById("exibirvalidade").innerHTML = document.querySelector("#validade")
@@ -68,7 +67,7 @@ document.getElementById("codigo").addEventListener("keyup", function (event) {
 
 IMask(document.querySelector("#cpf"), {
     mask: "000.000.000-00",
-  });
+});
 
 document.getElementById("cpf").addEventListener("keyup", function (event) {
     let cpf = document.getElementById("cpf").value;
@@ -82,5 +81,5 @@ document.getElementById("cpf").addEventListener("keyup", function (event) {
 
 
 
-  
+
 
